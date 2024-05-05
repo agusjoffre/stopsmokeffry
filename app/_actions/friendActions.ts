ab88f1044c339d8a886bfc7611e77900dbbfa3cf
agusjoffre <aguslj16@gmail.com>
2024-05-05 02:58:48 -0300

    create friendship

--- app/_actions/friendActions.ts
+++ app/_actions/friendActions.ts
@@ -1,10 +1,51 @@
 "use server";
-
+import { useToast } from "@/components/ui/use-toast";
+import prisma from "@/prisma/prismaClient";
 import { currentUser } from "@clerk/nextjs/server";
 
-const addFriend = async (formData: FormData) => {
+/* currentState is required for useFormState() hook*/
+export const addFriend = async (currentState: unknown, formData: FormData) => {
   const user = await currentUser();
   if (!user) return;
 
-  const friendCode = formData.get("friendCode");
+  const friendCode = formData.get("friendCode") as string;
+
+  const friend = await prisma.user.findFirst({
+    where: {
+      code: friendCode,
+    },
+  });
+
+  if (!friend) {
+    return {
+      found: false,
+      exists: false,
+    };
+  }
+
+  const existingFriendship = await prisma.friendship.findFirst({
+    where: {
+      friendId: friend.id,
+      friendOfId: user.id,
+    },
+  });
+
+  if (existingFriendship) {
+    return {
+      found: true,
+      exists: true,
+    };
+  }
+
+  const friendship = await prisma.friendship.create({
+    data: {
+      friendId: friend.id,
+      friendOfId: user.id,
+    },
+  });
+
+  return {
+    found: true,
+    exists: false,
+  };
 };
--- components/add-friend-form.tsx
+++ components/add-friend-form.tsx
@@ -1,12 +1,43 @@
+"use client";
+import { addFriend } from "@/app/_actions/friendActions";
 import SubmitButton from "./submit-btn";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { useFormState } from "react-dom";
+import { useEffect } from "react";
+import { useToast } from "./ui/use-toast";
 
 type Props = {};
 
 function AddFriendForm({}: Props) {
+  const [state, formAction] = useFormState(addFriend, null);
+
+  const { toast } = useToast();
+  useEffect(() => {
+    if (!state) return;
+    if (state.found === false) {
+      toast({
+        title: "Usuario no encontrado",
+        variant: "destructive",
+        duration: 2000,
+      });
+    } else if (state.found === true && state.exists === true) {
+      toast({
+        title: "Ya son amigos",
+        variant: "destructive",
+        duration: 2000,
+      });
+    } else {
+      toast({
+        title: "Solicitud enviada",
+        className: "bg-emerald-800 text-white",
+        duration: 1000,
+      });
+    }
+  }, [state]);
+
   return (
-    <form action="" className="flex flex-col gap-4">
+    <form action={formAction} className="flex flex-col gap-4">
       <Label className="flex flex-col gap-2">
         El código que te envió tu amigo/a
         <div className="flex gap-1">
@@ -17,7 +48,7 @@ function AddFriendForm({}: Props) {
             'Agregar amigo/a'
           </span>
         </div>
-        <Input name="friendCode" />
+        <Input name="friendCode" required />
       </Label>
       <SubmitButton text="Enviar solicitud" />
     </form>
